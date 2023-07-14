493e0fe336c36add2401b8dd59becdbb4179d5a7
DANELLE19 <debruyndanelle@gmail.com>
2023-07-14 12:15:37 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - Prints the numbers from 0 to 9 followed by alphabet letters 'a' to 'f'
+* main - Prints numbers between 0 to 9 and alphabet letters between 'a' to 'f'.
 * Return: Always 0 (success)
 */
 
