d4f6683c1e6590b3943354da638c17fb33d981fa
DANELLE19 <debruyndanelle@gmail.com>
2023-07-27 11:47:48 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,9 +13,9 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] -s2[i]);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
-	return (s1[i] -s2[i]);
+	return (s1[i] - s2[i]);
 }
