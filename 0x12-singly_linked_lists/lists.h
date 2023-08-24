d4c22e7dc30236b26b27fd5e7d8ab2d73344fff7
Danelle <debruyndanelle@gmail.com>
2023-08-24 13:52:33 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
 #ifndef LISTS_H
-#define LISTS.H
+#define LISTS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singliy linked list
  * @str: string - (malloc'ed string)
